fc48ad74ad1b9c21df09114f8dba8f8a2b796add
briorda <35381658+briorda@users.noreply.github.com>
2019-12-06 15:38:04 -0500

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,198 +1,135 @@
-/* priority.c */
+/* priority.h */
 
-#include "priority.h"
-#include "sim.h"
-#include "event.h"
-#include "queue.h"
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
-struct priority_s { //typedef removed
-    int current_size; //current size of the heap-array
-    int max_size;   //max size of the heap-array
-    event_t **event_tree;
-}; //priority_t removed
+// #include "event.h"
+#include "priority.h"
+
+#define PRISIZE 100
+
+/* the priority queue type */
+typedef struct priority_s priority_t;
+struct priority_s
+{
+    int size;
+    int count;
+    cmp_t cmp;
+    p_data_t *A[PRISIZE];
+};
+
+/* type cmp_t is a pointer to a function that returns in and takes 
+   two pointers for arguments. computes less than of the two
+   pointer's value */
+typedef int (* cmp_t)(p_data_t *left, p_data_t *right);
 
 /* create and initialize a new priority queue 
    must be able to hold at least size items
    return pointer to the new priority queue, NULL if error */
-priority_t *priority_init(int size) {
-    if(size < 0) {
-        printf("Size < 0\n");
-        return NULL;
-    }
-    
-    priority_t *q = (priority_t *)malloc(sizeof(priority_t));
-    q->max_size = size; // maximum size of tree
-    q->current_size = 0;   // number of nodes in tree
-    q->event_tree = (event_t **)malloc(sizeof(event_t)*(size+1)); // tree
-    
-    for(int i = 0; i < size; i++) {
-        q->event_tree[i] = NULL;
+priority_t *priority_init(int size, cmp_t cmp)
+{
+    priority_t *pri = (priority_t *)malloc(sizeof(priority_t));
+    memset(pri, 0, sizeof(priority_t));
+    pri->size = PRISIZE;
+    pri->count = 0; 
+    return pri;
+}
+
+void priority_insert(priority_t *pri, p_data_t *pd)
+{
+    int child, parent;
+
+    if (priority_full(pri))
+    {
+        return;
     }
 
-    if(q == NULL) {
-        printf("Cannot Intialize Priority Queue\n");
+    //printf("PRI INSERT %f\n", pd->event_time);
+    pri->count++;
+    child = pri->count;
+    parent = child/2;
+
+    while(parent != 0)
+    {
+        if(pri->cmp(pd, pri->A[parent]))
+        {
+            pri->A[child] = pd;
+            return;
+        }
+        else
+        {
+            pri->A[child] = pri->A[parent];
+            child = parent;
+            parent = parent/2;
+        }
+    }
+    pri->A[child] = pd;
+}
+
+/* remove the highest priority item from the queue 
+   and return it, return NULL if there is an error */
+p_data_t *priority_remove(priority_t *pri)
+{
+    int current = 1;
+    int child = 2;    /* 2 times current */
+
+    if (priority_empty(pri))
+    {
+        /* error - empty */
         return NULL;
     }
 
-    return q;
+    p_data_t *val = pri->A[1];
+    p_data_t *place = pri->A[pri->count];
+    pri->count--;
+
+    while(child <= pri->count)
+    {
+        if (child < pri->count)
+        {
+            if (pri->cmp(pri->A[child + 1], pri->A[child]))
+            {
+                child++;
+            }
+        }
+        if (pri->cmp(pri->A[child], place))
+        {
+            pri->A[current] = place;
+            return val;
+        }
+        else
+        {
+            pri->A[current] = pri->A[child];
+            current = child;
+            child = 2 * current;
+        }
+    }
+
+    pri->A[current] = place;
+    // pri->A[pri->count + 1] = NULL;
+    // printf("PRI RETURNS %f\n", val->event_time);
+    return val;
 }
 
 /* return non-zero if the priority queue us empty */
-int priority_empty(priority_t *q) {
-    int isEmpty = -1;
-
-    if(q->current_size == 0) {
-        isEmpty = 1;
-        // printf("Priority Queue currently empty\n");
-    } else {
-        isEmpty = 0;
-    }
-
-    return isEmpty;
+int priority_empty(priority_t *pri)
+{
+    return (pri->count == 0);
 }
 
 /* return nono-zero if the priority queue is full 
    This may be trivial using a linked implementation */
-int priority_full(priority_t *q) {
-    int isFull = 0;
-
-    if(q->current_size == q->max_size) {
-        isFull = 1;
-        isPQFull = 1;
-        printf("Priority Queue Full\n");
-    }
-
-    return isFull;
-}
-
-//Function to make the array into a heap
-static void heapify(priority_t *q, event_t *ev)
-{//increasing the size of the array by 1 since the first element in the array needs to be empty
-    q->current_size++;
-    int child_pos, parent_pos;
-    child_pos = q->current_size;
-    parent_pos = child_pos/2;
-
-    //if the parent position is the zero'th element, then skip this loop since it should be empty
-    while(parent_pos != 0)
-    {//if the event time is greater than the parent's event time, make this the chid's value since this is an
-     //array that should have small values higher in the heap as opposed to large values
-        if(ev->event_time >= q->event_tree[parent_pos]->event_time)
-        {//set the child's priority value
-            q->event_tree[child_pos] = ev;
-            return;
-        }
-        else
-        {//if the function makes it to this part, that means the parent is larger than the child (which shouldn't be the case)
-         //sets the child as the old's parents position and moves the parent's position up a level
-            q->event_tree[child_pos] = q->event_tree[parent_pos];
-            child_pos = parent_pos;
-            parent_pos = parent_pos/2;
-        }
-    }//set the child's priority value
-    q->event_tree[child_pos] = ev;
-}
-
-/* insert an item into the priority queue 
-   return 0 if successful, -1 otherwise */
-int priority_insert(priority_t *q, event_t *ev) {
-    int full, empty, success;
-
-    full = priority_full(q);
-    empty = priority_empty(q);
-    success = 1;
-
-    if(full == 1) {
-        isPQFull = 1;
-        success = -1;
-    } else if(empty == 1) {
-        q->current_size++;
-        q->event_tree[1] = ev;
-        success = 0;
-    } else {
-        heapify(q, ev);
-        success = 0;
-    }
-
-    return success;
-}
-
-// function to reheapify the pq after a removal
-static void reheapify(priority_t *q) {
-    int current_pos = 1;
-    int child_pos = 2*current_pos;
-    event_t *ev;
-    ev = q->event_tree[q->current_size];
-    q->event_tree[q->current_size] = NULL;
-    q->current_size--;
-
-    if(priority_empty(q) != 1) {
-        while(child_pos <= q->current_size) {
-            if(child_pos < q->current_size) {
-                double right_child = q->event_tree[child_pos+1]->event_time;
-                double left_child = q->event_tree[child_pos]->event_time;
-
-                if(right_child < left_child)
-                    child_pos++;
-            }
-
-            if(q->event_tree[child_pos]->event_time >= ev->event_time)
-                break;
-            else {
-                q->event_tree[current_pos] = q->event_tree[child_pos];
-                current_pos = child_pos;
-                child_pos = 2*child_pos;
-            }
-            
-        }
-        q->event_tree[current_pos] = ev;
-    }
-}
-
-//remove the root (highest priority) of the heap
-event_t *priority_remove(priority_t *q) {
-    int empty = priority_empty(q);
-    event_t *removal = event_create();
-    
-    if(empty == 1) {
-        removal = NULL;
-    } else {
-        removal = q->event_tree[1];
-        q->event_tree[1] = q->event_tree[q->current_size];
-        reheapify(q);
-    }
-
-    return removal;
-}
-
-// function to print the current heap
-void print_heap(priority_t *q) {
-    int isEmpty = priority_empty(q);
-    
-    if(isEmpty) {
-        return;
-    } else {
-        for(int i = 1; i <= q->current_size; i++) {
-            printf("%f\n", q->event_tree[i]->event_time);
-        }
-        printf("\n");
-    }
+int priority_full(priority_t *pri)
+{
+    return (pri->count == pri->size);
 }
 
 /* free all resourced used by the priority queue then free
    the queue itself */
-void priority_finalize(priority_t *q) {
-    for(int i = 0; i < q->current_size; i++) {
-        free(q->event_tree[i]);
-        q->event_tree[i] = NULL;
-    }
-    
-    free(q->event_tree);
-    q->event_tree = NULL;
-
-    free(q);
-    q = NULL;
+void priority_finalize(priority_t *pri)
+{
+    free(pri);
 }
+
