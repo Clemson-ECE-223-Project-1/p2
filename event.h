a46ad0c68ef54747c1f2de678ad72d30df82e1cf
briorda <35381658+briorda@users.noreply.github.com>
2019-12-06 15:37:33 -0500

    Update event.h

--- event.h
+++ event.h
@@ -1,24 +1,26 @@
 /* event.h */
 
-typedef struct event_s event_t;
-typedef struct queue_s queue_t;
-typedef struct priority_s priority_t;
-typedef struct passenger_s passenger_t;
+#ifndef EVENT_H
+#define EVENT_H
 
-struct event_s { //typedef removed
-   int event_type;        /* type of event â€“ see below */
-   queue_t *queue;        /* queue passenger is waiting in */
+#include "sim.h"
+
+typedef struct event_s event_t;
+struct event_s
+{
+   int event_type;        /* type of event – see below */
+   // queue_t *queue      /* queue passenger is waiting in */
    double event_time;     /* sim time when event occurs */
    passenger_t *passenger;/* passenger related to this event */
-}; //event_t;
+};
 
-double event_time(event_t *e);
+/* initializes events, creates a priority queue 
+   including the size of the queue */
+void event_init(int size);
 
-/* initializes events, creates a priority queue */
-void event_init();
-
-/* frees up all event space, including space in the priority queue */
-void event_fini();
+/* frees up all event space, including space in the priority
+   queue */
+void event_finalize();
 
 /* allocate a fresh event with empty fields */
 event_t *event_create();
@@ -29,11 +31,16 @@ void event_destroy(event_t *e);
 /* insert the event into the priority queue.  The key
    value is the current sim time plus the event_time in
    event.  Update the event time to the key value. */
-void event_schedule(event_t *e);
+void event_schedule(event_t *e); 
 
-/* remove the next event from the priority and return it to the program for execution */
+/* returns 0 if there are more events in the event queue, and non zero
+   if there are no more events. */
+int event_empty();
+
+/* remove the next event from the priority queue, set the time to the event’s time, and then return it to the program for execution */
 event_t *event_cause();
 
-// function to check if event is empty
-// returns 1 if empty, 0 if not
-int event_empty(event_t *e);
+/* returns true if *a is less than *b callback for priority queue */
+int event_cmp(event_t *a, event_t *b);
+
+#endif
