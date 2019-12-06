0276a456156a727e19dbb5df7c9b9d1b53f7c272
briorda <35381658+briorda@users.noreply.github.com>
2019-12-06 15:38:25 -0500

    Update priority.h

--- priority.h
+++ priority.h
@@ -1,30 +1,41 @@
 /* priority.h */
 
-typedef struct priority_s priority_t;
-typedef struct event_s event_t;
+#ifndef PRIORITY_H
+#define PRIORITY_H
 
-extern int isPQFull;
+/* the priority queue type */
+typedef struct priority_s priority_t;
+
+/* this is they type of items put in the queue, opaque to the queue */
+typedef struct priority_data p_data_t;
+
+/* type lt_t is a pointer to a function that returns in and takes 
+   two pointers for arguments. computes less than of the two
+   pointer's value */
+typedef int (* cmp_t)(p_data_t *left, p_data_t *right);
 
 /* create and initialize a new priority queue 
    must be able to hold at least size items
    return pointer to the new priority queue, NULL if error */
-priority_t *priority_init(int size);
+priority_t *priority_init(int size, cmp_t cmp);
 
 /* insert an item into the priority queue 
    return 0 if successful, -1 otherwise */
-int priority_insert(priority_t *q, event_t *ev);
+void priority_insert(priority_t *pri, p_data_t *pd);
 
 /* remove the highest priority item from the queue 
    and return it, return NULL if there is an error */
-event_t *priority_remove(priority_t *q);
+p_data_t *priority_remove(priority_t *pri);
 
 /* return non-zero if the priority queue us empty */
-int priority_empty(priority_t *q);
+int priority_empty(priority_t *pri);
 
 /* return nono-zero if the priority queue is full 
    This may be trivial using a linked implementation */
-int priority_full(priority_t *q);
+int priority_full(priority_t *pri);
 
 /* free all resourced used by the priority queue then free
    the queue itself */
-void priority_finalize(priority_t *q);
+void priority_finalize(priority_t *pri);
+
+#endif
