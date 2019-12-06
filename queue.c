d394ff9697c0e89fc7bb928a76c08de4b2317c02
briorda <35381658+briorda@users.noreply.github.com>
2019-12-06 15:38:40 -0500

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,99 +1,118 @@
-/* queue.c */
+/* queue.h */
 
-#include "queue.h"
-#include "event.h"
-#include "sim.h"
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
-/* check to see if queue is empty */
-int queue_empty(queue_t *q) {
-	if (q->front == NULL) {
-       printf("Queue is empty.\n");
-	   return 1;
-   } else {
-	   printf("Queue is not empty.\n");
-	   return 0;
-   }
+#include "sim.h"
+#include "queue.h"
+
+typedef struct link_s link_t;
+struct link_s
+{
+    q_data_t *data;
+    link_t *link;
+};
+
+struct queue_s
+{
+    int numitems;
+    link_t *head;  /* remove here */
+    link_t *tail;  /* insert here */
+};
+
+/* create and initialize a new queue 
+   must be able to hold at least size items
+   return pointer to the new queue, NULL if error */
+queue_t *queue_init(int size)
+{
+    queue_t *q = (queue_t *)malloc(sizeof(queue_t));
+    memset(q, 0, sizeof(queue_t));
+    return q;
 }
 
-/*	initialize queue by setting count to 0
-	and pointing the head and tail to NULL */
-queue_t *queue_init(int size) {
-	queue_t  *q = (queue_t *)malloc(size*sizeof(queue_t));
-	q->count = 0;
-	q->front = NULL;
-	q->rear = NULL;
-	return (q);
-}
-
-/* function to add elements to the queue from the rear */
-int queue_insert(queue_t *q, passenger_t *c) {
-	node *temp; 							// create a new node to add to the queue
-	temp = (node *)malloc(sizeof(node));	// request memory for the node
-	temp->passenger = c;
-	temp->next = NULL;
-
-	// if queue not empty, add to end of queue
-	if (queue_empty(q) == 0) {
-		q->rear->next = temp;
-		q->rear = temp;
-	// if queue is full, can't add to queue
-	} else if(queue_full(q) != 0) {
-		return 1;
-	// if empty, insert first node into queue
-	} else {
-		q->front = q->rear = temp;
-	}
-	q->count++;							// update size of queue
-	return 0;
+void queue_insert(queue_t *q, q_data_t *dat)
+{
+    if (q->head == NULL && q->tail == NULL)
+    {
+        q->head = (link_t *)malloc(sizeof(link_t));
+        q->tail = q->head;
+    }
+    else
+    {
+        q->tail->link = (link_t *)malloc(sizeof(link_t));
+        q->tail = q->tail->link;
+    }
+    q->tail->link = NULL;
+    q->tail->data = dat;
+    q->numitems++;
 }
 
 /* return the next item in the queue but do not remove it
    return NULL if the queue is empty or on error */
-passenger_t *queue_peek(queue_t *q) {
-	return q->front->passenger;
+q_data_t *queue_peek(queue_t *q)
+{
+    if (q->numitems == 0 || q->head == NULL || q->tail == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        return q->head->data;
+    }
 }
 
-/* function to delete the front node and return its value */
-passenger_t *queue_remove(queue_t *q) {
-	passenger_t *removed_passenger;
-	if (queue_empty(q) == 1) {
-		printf("Error: Cannot remove from empty queue!\n");
-		return NULL;
-   	} else {
-		node *newFront = q->front;
-		q->front = q->front->next;
-		q->count--;
-		removed_passenger = newFront->passenger;
-		free(newFront);
-		return (removed_passenger);
-	}
+/* remove the next item from the queue 
+   and return it, return NULL if there is an error */
+q_data_t *queue_remove(queue_t *q)
+{
+    link_t *next;
+    if (q->numitems == 0 || q->head == NULL || q->tail == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        next = q->head;
+        q_data_t *next_d;
+        if (q->tail == q->head)
+        {
+            q->tail = q->head = NULL;
+        }
+        q->head = q->head->link;
+        q->numitems--;
+        next_d = next->data;
+        free(next);
+        return next_d;
+    }
 }
-
 /* return the number of items in the queue 
    You can see if queue is empty with this */
-int queue_size(queue_t *q) {
-    printf("Size of Queue = %d\n", q->count);
-	return q->count;
+int queue_size(queue_t *q)
+{
+    return q->numitems;
 }
 
-/* check to see if queue is full */
-int queue_full(queue_t *q) {
-	if (q->count == MAX_PASS) {
-       printf("Queue is full.\n");
-	   return 1;
-   } else {
-	   printf("Queue is not full.\n");
-	   return 0;
-   }
+/* return nono-zero if the queue is full
+   This may be trivial using a linked implementation */
+int queue_full(queue_t *q)
+{
+    return (0);
 }
 
 /* free all resourced used by the queue then free
    the queue itself */
-void queue_finalize(queue_t *q) {
-	free(q->front);
-	free(q->rear);
-	free(q);
+void queue_finalize(queue_t *q)
+{
+    link_t *rover = q->head;
+    link_t *item = rover;
+    while(rover)
+    {
+        item = rover;
+        rover = rover->link;
+        free(item);
+    }
+    free(q);
 }
+
